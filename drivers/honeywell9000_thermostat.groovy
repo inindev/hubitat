c60a4e8bf56ad39efc7438a43dd8480241afec7a
John Clark <inindev@gmail.com>
2020-10-08 06:27:39 -0400

    config variables

--- drivers/honeywell9000_thermostat.groovy
+++ drivers/honeywell9000_thermostat.groovy
@@ -1,8 +1,22 @@
-/*
+/**
  *  honeywell9000_thermostat.groovy
  *
  *    Hubitat support for the Honeywell WiFi 9000 Thermostat
  *    https://www.honeywellhome.com/us/en/products/air/thermostats/wifi-thermostats/wifi-9000-color-touchscreen-thermostat-th9320wf5003-u/
+ *
+ *    Copyright 2000 John Clark
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  *
  */
 
@@ -12,9 +26,12 @@ metadata {
             description: 'Hubitat driver for the Honeywell WiFi 9000 Thermostat',
             namespace: 'com.github.inindev',
             author: 'John Clark',
-            importUrl: 'https://raw.githubusercontent.com/inindev/hubitat/main/drivers/honeywell9000_thermostat.groovy'
+            importUrl: 'https://raw.githubusercontent.com/inindev/hubitat/main/drivers/honeywell9000_thermostat.groovy',
     ) {
         capability 'Thermostat'
+        capability 'Sensor'
+        capability 'TemperatureMeasurement'
+        capability 'RelativeHumidityMeasurement'
     }
 }
 
@@ -22,6 +39,13 @@ preferences {
     input name: 'logInfo', type: 'bool', title: 'Enable info logging', defaultValue: true
     input name: 'logDebug', type: 'bool', title: 'Enable debug logging', defaultValue: false
 }
+
+@groovy.transform.Field static final Map config = [
+        uri_tcc_site: 'https://mytotalconnectcomfort.com',
+        path_portal : '/portal/',
+        path_gzld   : '/portal/Device/GetZoneListData',
+        path_scsc   : '/portal/Device/SubmitControlScreenChanges',
+]
 
 
 /**
@@ -84,18 +108,18 @@ def fanOn() {
 
 /**
  * Thermostat::setCoolingSetpoint
- * @param temp - cooling setpoint in degrees F/C
+ * @param temperature - cooling setpoint in degrees C/F
  */
 def setCoolingSetpoint(java.math.BigDecimal temperature) {
-    if(logInfo) log.info "${myLabel}  - temperature: ${temperature}"
+    if(logInfo) log.info "${myLabel}  Thermostat::setCoolingSetpoint - temperature: ${temperature}"
 }
 
 /**
  * Thermostat::setHeatingSetpoint
- * @param temp - heating setpoint in degrees F/C
+ * @param temperature - heating setpoint in degrees C/F
  */
 def setHeatingSetpoint(java.math.BigDecimal temperature) {
-    if(logInfo) log.info "${myLabel}  - temperature: ${temperature}"
+    if(logInfo) log.info "${myLabel}  Thermostat::setHeatingSetpoint - temperature: ${temperature}"
 }
 
 /**
@@ -103,7 +127,7 @@ def setHeatingSetpoint(java.math.BigDecimal temperature) {
  * @param schedule - schedule to set (json string)
  */
 def setSchedule(String schedule) {
-    if(logInfo) log.info "${myLabel}  - schedule: ${schedule}"
+    if(logInfo) log.info "${myLabel}  Thermostat::setSchedule - schedule: ${schedule}"
 }
 
 /**
@@ -111,7 +135,7 @@ def setSchedule(String schedule) {
  * @param mode - fan mode to set: 'on', 'circulate', 'auto'
  */
 def setThermostatFanMode(String mode) {
-    if(logInfo) log.info "${myLabel}  - mode: ${mode}"
+    if(logInfo) log.info "${myLabel}  Thermostat::fanmode - mode: ${mode}"
 }
 
 /**
@@ -119,7 +143,7 @@ def setThermostatFanMode(String mode) {
  * @param mode - thermostat mode to set: 'off', 'cool', 'heat', 'emergency heat', 'auto'
  */
 def setThermostatMode(String mode) {
-    if(logInfo) log.info "${myLabel}  - mode: ${mode}"
+    if(logInfo) log.info "${myLabel}  Thermostat::setThermostatMode - mode: ${mode}"
 }
 
 
